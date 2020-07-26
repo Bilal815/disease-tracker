58ae804b61fb5414e4b12dabc0d5c92df64b2c41
Bilal815 <62904483+Bilal815@users.noreply.github.com>
2020-07-26 11:27:57 +0500

    Upload All Files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import {ManuItem, 
+        FormControl,
+        Select}
+        from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return ( 
+    <div className="app" > { /*BEM naming convention*/ } 
+      <div className="app__header">
+        <h1>COVID 19 Tracker</h1> 
+        <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">YOOOOOOOO</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+        
+        {/* Header */}
+        {/* Title + Select Input Dropdown Field*/}
+
+        {/*InfoBox*/}
+        {/*InfoBox*/}
+        {/*InfoBox*/}
+
+        {/* Table */}
+        {/* Graph */}
+
+        {/* Map */}
     </div>
-  );
+    );
 }
 
 export default App;
